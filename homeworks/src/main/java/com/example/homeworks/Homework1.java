628aa1a4c783b85d7eec03db658e7ff052faef09
Chea <cwright2020@ihs.immaculateheart.org>
2019-08-30 15:17:21 -0700

    Final Homework 1

--- homeworks/src/main/java/com/example/homeworks/Homework1.java
+++ homeworks/src/main/java/com/example/homeworks/Homework1.java
@@ -7,6 +7,8 @@ public class Homework1 {
 
         String age = "sixteen";
 
+        boolean sameLength = name == age;
+
         System.out.println("My name is " + name + " and my age is " + age);
 
         System.out.println("In uppercase, my name is " + name.toUpperCase());
@@ -14,6 +16,12 @@ public class Homework1 {
         System.out.println("In lowercase, my name is " + name.toLowerCase());
 
         System.out.println("My name is " + name.length() + " characters long");
+
+        System.out.println("My age is " + age.length() + " characters long");
+
+       System.out.println("Does my name equal my age? " + sameLength);
+
+        System.out.println("My name is still " + name);
 
     }
 }
